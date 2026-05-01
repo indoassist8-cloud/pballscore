f05e30c58b21f15a361686ae6bcdd26b42cb576a
Indo Assists 8 <indoassist8@gmail.com>
2026-05-01 14:14:36 +0700

    update tailwind bg color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}", "./public/*.html"],
+  safelist: [
+    'bg-red-100',
+    'text-red-700',
+    'border-red-400',
+    'bg-green-100',
+    'text-green-700',
+    'border-green-400',
+  ],
   theme: {
     extend: {
       "colors": {
